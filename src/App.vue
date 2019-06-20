2847d60ab2f8bfb80d4507ac2950713547b881de
lebmouse <gtah2yk@gmail.com>
2019-06-20 15:00:37 +0900

    hello vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello vue.js!!!
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
