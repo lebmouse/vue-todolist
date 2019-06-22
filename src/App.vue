cd0e43a32a6a5cc92fa56ff0b79cf2690a0375fa
lebmouse <gtah2yk@gmail.com>
2019-06-22 16:22:20 +0900

    compelte c05 vuex

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,31 @@
 <template>
   <div id="app">
-    <header>Todo List</header>
-    <section class="input">
-      <input type="text" v-model="value" @keyup.enter="addItem">
-      <button @click="addItem">제출</button>
-    </section>
-    <section>
-      <ul>
-        <li v-for="(item,index) in list" :key="index">
-          <input class="item" ref="item" readonly type="text" :value="item">
-          <button ref="modifyBtn" @click="modifyItem(index)">수정</button>
-          <button @click="removeItem(index)">제거</button>
-        </li>
-      </ul>
-    </section>
+    <Header></Header>
+    <InputBar></InputBar>
+    <List></List>
   </div>
 </template>
 
 <script>
-const STORAGE_KEY = "todoList";
-var todoStorage = {
-  fetch: function() {
-    var todos = localStorage.getItem(STORAGE_KEY) || "[]";
-    todos = JSON.parse(todos);
-    return todos;
-  },
-  save: function(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-  }
-};
+import Header from "./components/Header.vue";
+import InputBar from "./components/InputBar.vue";
+import List from "./components/List.vue";
 
 export default {
   name: "app",
-  data() {
-    return {
-      value: "",
-      valueToModify: "",
-      prevModifiedItem: null,
-      presentModifiedItem: null,
-      list: todoStorage.fetch()
-    };
+  components: {
+    Header,
+    InputBar,
+    List
   },
-  watch: {
-    list: function(todos) {
-      console.log("리스트변경");
-      todoStorage.save(todos);
-    }
+  created() {
+    this.$store.dispatch("asynSetList");
   },
-  // 5. methods
-  methods: {
-    addItem() {
-      if (this.value) {
-        this.list.push(this.value);
-        this.value = "";
-      }
-    },
-    removeItem(index) {
-      this.list.splice(index, 1);
-    },
-    modifyItem(index) {
-      let prev = this.prevModifiedItem;
-      if (prev !== null) {
-        let item = this.$refs["item"][prev];
-        let modifyBtn = this.$refs["modifyBtn"][prev];
-        item.setAttribute("readonly", "readonly");
-        modifyBtn.innerText = "수정";
-      }
-      if (this.prevModifiedItem === index) {
-        let item = this.$refs["item"][index];
-        let modifyBtn = this.$refs["modifyBtn"][index];
-        item.setAttribute("readonly", "readonly");
-        modifyBtn.innerText = "수정";
-        this.list.splice(index, 1, item.value);
-        console.log("수정완료");
-        this.prevModifiedItem = null;
-      } else {
-        let item = this.$refs["item"][index];
-        let modifyBtn = this.$refs["modifyBtn"][index];
-        item.removeAttribute("readonly");
-        modifyBtn.innerText = "완료";
-        this.presentModifiedItem = index;
-        this.prevModifiedItem = index;
-      }
-    }
+  mounted() {
+    this.$store.watch(
+      () => this.$store.getters.getList,
+      todos => localStorage.setItem("todoList", JSON.stringify(todos))
+    );
   }
 };
 </script>
